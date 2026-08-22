6dfa23c70c3e6c99de75af8361fcb49e91e31458
Imtiaz4530 <apual4016@gmail.com>
2026-08-23 05:39:21 +0600

    initial admin site created

---
+++ admin/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="w-full h-[100vh] bg-blue-400">
+      <h1 className="text-3xl text-red-400">HELLO, WORLD</h1>
+    </div>
+  );
+};
+
+export default App;
