030b9ec9dcf02da9d54fd7a3a3c2f8058da00e77
Imtiaz4530 <apual4016@gmail.com>
2026-09-03 08:39:11 +0600

    back to previous route

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-import path from "path";
 const dotenv = require("dotenv");
 
 const connectDB = require("./config/db");
@@ -23,8 +22,8 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
-app.get("/", (_req, res) => {
-  res.sendFile(path.join(process.cwd(), "public", "api-status.html"));
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
 
 app.get("/api/health", (req, res) => {
